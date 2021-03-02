8a9c789db61be42beca543ce3ffcf5092e5271e5
JJ Falcon <inbox4development@gmail.com>
2021-03-02 03:54:14 +0000

    Code psql_docker.sh

---
+++ linux_sql/scripts/psql_docker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#check if docker is running, else start docker
+sudo systemctl status docker || systemctl start docker
+
+case $1 in
+	create)
+	  #check if docker container is alrady created
+	  if [ `docker container ls -a -f name=jrvs-psql | wc -l` -eq 2 ]; then
+	  	echo "container already created"
+  		echo "exiting"
+  		exit 1;
+	  fi
+
+	  #check if log-in info are supplied
+	  if [ $2 = "" || $3 = "" ]; then
+		echo "missing user log-in info"
+  		echo "exiting"
+  		exit 1;
+	  fi
+
+	  #create a new volume
+	  docker volume create pgdata
+
+	  #create a new psql container
+	  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+
+	  #check if last command was a success
+	  if [ $? -eq 1 ]; then
+		echo "error creating container"
+  		echo "exiting"
+  		exit 1;
+	  fi
+	;;
+
+	start)
+	  docker container start jrvs-psql
+	  exit $?
+	;;
+
+	stop)
+	  docker container stop jrvs-psql
+	  exit $?
+	;;
+
+  #else any other argument is invalid
+	*)
+    echo -e "invalid argument\n"
+    exit 1
+  ;;
+esac

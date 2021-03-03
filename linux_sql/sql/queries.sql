2a32f0bb25af15166564353b3fbce4b7ad207b73
JJ Falcon <inbox4development@gmail.com>
2021-03-03 03:52:36 +0000

    Edit Formatting

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,8 +1,9 @@
 -- sample queries
 
 -- list the host information by their memory size in descending order for each cpu number
-SELECT	cpu_number, host_info_id, total_mem, avg(total_mem) OVER (PARTITION BY cpu_number) as avg_mem
-FROM	sample_info ORDER BY cpu_number ASC, total_mem DESC;
+SELECT		cpu_number, host_info_id, total_mem, avg(total_mem) OVER (PARTITION BY cpu_number) as avg_mem
+FROM		sample_info
+ORDER BY 	cpu_number ASC, total_mem DESC;
 
 -- create function that rounds the time stamp to every 5 minutes
 CREATE FUNCTION round5s(ts timestamptz) RETURNS timestamptz AS
@@ -22,10 +23,13 @@ FROM
 	USING 		(host_info_id)) AS temp;
 
 -- list the host number of the hosts that failed to insert at least 3 entries to the host_usage table every 5 mintues
-SELECT 	host_number, count from (select host_number, count(per5) as count
+SELECT 	host_number, count
 FROM
-	(SELECT host_number, round5s(time_capture) as per5
-	FROM 		sample_info) as temp
-	GROUP BY 	host_number, per5
-	ORDER BY 	host_number) as newTemp
-	WHERE 		count < 3;
+	(SELECT 	host_number, count(per5) as count
+	FROM
+		(SELECT 	host_number, round5s(time_capture) as per5
+		FROM 		sample_info) as temp
+		GROUP BY 	host_number, per5
+		ORDER BY 	host_number) as newTemp
+		WHERE 		count < 3;
+
